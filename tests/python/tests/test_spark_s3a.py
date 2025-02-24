55a5334c35cab649a72cb218aa781d2b379614be
Christian <christian@vakamo.com>
2025-02-24 09:40:45 +0100

    feat: Update Storage Profile alternative S3 protocols in UI (#848)

--- tests/python/tests/test_spark_s3a.py
+++ tests/python/tests/test_spark_s3a.py
@@ -1,3 +1,4 @@
+import copy
 import json
 
 import conftest
@@ -106,3 +107,74 @@ def test_register_table(
         f"SELECT * FROM {namespace.spark_name}.my_registered_table"
     ).toPandas()
     assert pdf["my_ints"].tolist() == [1]
+
+
+def test_disable_alternative_protocols(
+    spark, warehouse: conftest.Warehouse, s3_warehouse_location, storage_config, server
+):
+    spark.sql("CREATE NAMESPACE test_disable_alternative_protocols")
+
+    s3a_path = f"{s3_warehouse_location}/test_disable_alternative_protocols".replace(
+        "s3://", "s3a://"
+    )
+    s3a_path = f"{s3a_path}-{int(time.time())}"
+
+    # Create regular table
+    spark.sql(
+        f"""CREATE TABLE test_disable_alternative_protocols.my_table 
+        (my_ints INT, my_floats DOUBLE, strings STRING) USING iceberg
+        LOCATION '{s3a_path}'
+        """
+    )
+
+    # Insert data
+    spark.sql(
+        f"""INSERT INTO test_disable_alternative_protocols.my_table 
+        VALUES (1, 1.1, 'a'), (2, 2.2, 'b')
+        """
+    )
+
+    # Read data
+    df = spark.sql(
+        "SELECT * FROM test_disable_alternative_protocols.my_table"
+    ).toPandas()
+    pd.testing.assert_frame_equal(
+        df,
+        pd.DataFrame(
+            {"my_ints": [1, 2], "my_floats": [1.1, 2.2], "strings": ["a", "b"]}
+        ),
+        check_dtype=False,
+    )
+
+    # Disable s3a
+    storage_config = copy.deepcopy(storage_config)
+    storage_config["storage-profile"]["allow-alternative-protocols"] = False
+    response = requests.post(
+        server.warehouse_url + f"/{warehouse.warehouse_id}/storage",
+        json=storage_config,
+        headers={"Authorization": f"Bearer {server.access_token}"},
+    )
+    response.raise_for_status()
+
+    # Test: Can still write to and update existing tables
+    spark.sql(
+        f"""INSERT INTO test_disable_alternative_protocols.my_table 
+        VALUES (3, 3.3, 'c')
+        """
+    )
+    df = spark.sql(
+        "SELECT * FROM test_disable_alternative_protocols.my_table"
+    ).toPandas()
+
+    # Test: cannot create new s3a tables
+    s3a_path = f"{s3_warehouse_location}/test_disable_alternative_protocols_2".replace(
+        "s3://", "s3a://"
+    )
+    s3a_path = f"{s3a_path}-{int(time.time())}"
+    with pytest.raises(Exception):
+        spark.sql(
+            f"""CREATE TABLE test_disable_alternative_protocols.my_table2 
+            (my_ints INT, my_floats DOUBLE, strings STRING) USING iceberg
+            LOCATION '{s3a_path}'
+            """
+        )
