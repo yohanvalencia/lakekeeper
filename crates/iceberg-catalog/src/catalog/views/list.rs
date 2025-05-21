28ae8eb8a0008647626cf0c9c0b10204b76454c1
Tobias Pütz <tobias@hansetag.com>
2025-05-21 12:17:25 +0200

    fix: missing namespace permissions should return forbidden (#1117)

--- crates/iceberg-catalog/src/catalog/views/list.rs
+++ crates/iceberg-catalog/src/catalog/views/list.rs
@@ -7,10 +7,12 @@ use crate::{
         iceberg::v1::{ListTablesQuery, NamespaceParameters, PaginationQuery},
         ApiContext, Result,
     },
-    catalog::{namespace::validate_namespace_ident, require_warehouse_id, tabular::list_entities},
+    catalog::{
+        namespace::authorized_namespace_ident_to_id, require_warehouse_id, tabular::list_entities,
+    },
     request_metadata::RequestMetadata,
     service::{
-        authz::{Authorizer, CatalogNamespaceAction, CatalogViewAction, CatalogWarehouseAction},
+        authz::{Authorizer, CatalogNamespaceAction, CatalogViewAction},
         Catalog, SecretStore, State, Transaction,
     },
 };
@@ -25,28 +27,22 @@ pub(crate) async fn list_views<C: Catalog, A: Authorizer + Clone, S: SecretStore
     // ------------------- VALIDATIONS -------------------
     let NamespaceParameters { namespace, prefix } = parameters;
     let warehouse_id = require_warehouse_id(prefix)?;
-    validate_namespace_ident(&namespace)?;
 
     // ------------------- AUTHZ -------------------
     let authorizer = state.v1_state.authz;
-    authorizer
-        .require_warehouse_action(
-            &request_metadata,
-            warehouse_id,
-            CatalogWarehouseAction::CanUse,
-        )
-        .await?;
+
     let mut t: <C as Catalog>::Transaction =
         C::Transaction::begin_read(state.v1_state.catalog).await?;
-    let namespace_id = C::namespace_to_id(warehouse_id, &namespace, t.transaction()).await; // We can't fail before AuthZ.
 
-    authorizer
-        .require_namespace_action(
-            &request_metadata,
-            namespace_id,
-            CatalogNamespaceAction::CanListViews,
-        )
-        .await?;
+    let _namespace_id = authorized_namespace_ident_to_id::<C, _>(
+        authorizer.clone(),
+        &request_metadata,
+        &warehouse_id,
+        &namespace,
+        CatalogNamespaceAction::CanListViews,
+        t.transaction(),
+    )
+    .await?;
 
     // ------------------- BUSINESS LOGIC -------------------
 
--- crates/iceberg-catalog/src/service/authz/mod.rs
+++ crates/iceberg-catalog/src/service/authz/mod.rs
@@ -486,7 +486,7 @@ where
         match namespace_id {
             Ok(None) => {
                 tracing::debug!("Namespace not found.");
-                Err(ErrorModel::not_found(msg, typ, None).into())
+                Err(ErrorModel::forbidden(msg, typ, None).into())
             }
             Ok(Some(namespace_id)) => {
                 if self
@@ -496,7 +496,7 @@ where
                     Ok(namespace_id)
                 } else {
                     tracing::trace!("Namespace action forbidden.");
-                    Err(ErrorModel::not_found(msg, typ, None).into())
+                    Err(ErrorModel::forbidden(msg, typ, None).into())
                 }
             }
             Err(e) => Err(ErrorModel::internal(msg, typ, e.error.source)
