e137a96bd98ae2b9c68f8ebf15ca67d68a529c98
Christian <christian@vakamo.com>
2025-05-26 15:09:48 +0200

    fix: Role Owners can use permissions of their role (#1122)

--- crates/iceberg-catalog/src/service/authz/implementations/openfga/migration.rs
+++ crates/iceberg-catalog/src/service/authz/implementations/openfga/migration.rs
@@ -8,7 +8,7 @@ use openfga_client::{
 use super::{OpenFGAError, OpenFGAResult, AUTH_CONFIG};
 
 pub(super) static ACTIVE_MODEL_VERSION: LazyLock<AuthorizationModelVersion> =
-    LazyLock::new(|| AuthorizationModelVersion::new(3, 0)); // <- Change this for every change in the model
+    LazyLock::new(|| AuthorizationModelVersion::new(3, 1)); // <- Change this for every change in the model
 
 fn get_model_manager(
     client: &BasicOpenFgaServiceClient,
@@ -23,11 +23,11 @@ fn get_model_manager(
         serde_json::from_str(include_str!(
             // Change this for backward compatible changes.
             // For non-backward compatible changes that require tuple migrations, add another `add_model` call.
-            "../../../../../../../authz/openfga/v3.0/schema.json"
+            "../../../../../../../authz/openfga/v3.1/schema.json"
         ))
         // Change also the model version in this string:
-        .expect("Model v3.0 is a valid AuthorizationModel in JSON format."),
-        AuthorizationModelVersion::new(3, 0),
+        .expect("Model v3.1 is a valid AuthorizationModel in JSON format."),
+        AuthorizationModelVersion::new(3, 1),
         // For major version upgrades, this is where tuple migrations go.
         None::<MigrationFn<_>>,
         None::<MigrationFn<_>>,
